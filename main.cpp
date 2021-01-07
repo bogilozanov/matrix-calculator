0bf214bb1cc40aab8d4746f59ca5797e512f074d
= <bl@auxcode.com>
2021-01-08 01:41:40 +0200

    setting up the project

--- main.cpp
+++ main.cpp
@@ -14,9 +14,116 @@
 */
 
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+void chooseOperation(int &operationNumber);
+void multiplyMatrixByScalar();
+void multiplyMatrixByMatrix();
+void findTheDeterminant();
+void divideMatrixByScalar();
+void findTheInverseMatrix();
+void matrixTransposition();
+void inputAgainIfLowerThanOne();
+
+
 int main()
 {
-    cout << "test";
+  int operationNumber;
+  fstream textFile("main_functions.txt", fstream::in);
+
+  if (!textFile.is_open())
+  {
+    cout << "Could't open the text file";
     return 0;
+  }
+  cout << "Choose an operation you want to use!" << endl;
+  cout << textFile.rdbuf();
+  cin >> operationNumber;
+  if (operationNumber > 6 || operationNumber < 0)
+  {
+    cout << "Not valid operation number!";
+    return 0;
+  }
+  chooseOperation(operationNumber);
+
+  return 0;
+}
+
+void inputAgainIfLowerThanOne(int &number)
+{
+  cout << "Input must be one or greater!\nInput again: ";
+  cin >> number;
+  cout << endl;
+}
+
+void multiplyMatrixByScalar()
+{
+  int scalar, matrixRows, matrixCols;
+  cout << "Input the scalar: ";
+  cin >> scalar;
+  cout << endl;
+  cout << "Input the matrix rows: ";
+  cin >> matrixRows;
+  cout << endl;
+  while (matrixRows < 1)
+  {
+    inputAgainIfLowerThanOne(matrixRows);
+  }
+  cout << "Input the matrix columns: ";
+  cin >> matrixCols;
+  cout << endl;
+  while (matrixCols < 1)
+  {
+    inputAgainIfLowerThanOne(matrixCols);
+  }
+  
+}
+
+void multiplyMatrixByMatrix()
+{
+}
+
+void findTheDeterminant()
+{
+}
+
+void divideMatrixByScalar()
+{
+}
+
+void findTheInverseMatrix()
+{
+}
+
+void matrixTransposition()
+{
+}
+
+void chooseOperation(int &operationNumber)
+{
+  if (operationNumber == 1)
+  {
+    multiplyMatrixByScalar();
+  }
+  else if (operationNumber == 2)
+  {
+    multiplyMatrixByMatrix();
+  }
+  else if (operationNumber == 3)
+  {
+    findTheDeterminant();
+  }
+  else if (operationNumber == 4)
+  {
+    divideMatrixByScalar();
+  }
+  else if (operationNumber == 5)
+  {
+    findTheInverseMatrix();
+  }
+  else if (operationNumber == 6)
+  {
+    matrixTransposition();
+  }
 }
