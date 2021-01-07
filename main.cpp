8dc618b46c0bdace07fbe1f103c8763d5ee28dae
Bogdan Lozanov <bl@auxcode.com>
2021-01-07 23:45:29 +0200

    test commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/**
+*  
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2020/2021
+*
+* @author Bogdan Lozanov
+* @idnumber 62592
+* @compiler GCC
+*
+* <main file>
+*
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "test";
+    return 0;
+}
