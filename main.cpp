22deb413d0ebce7afabebacf3df8314062349ea7
= <bl@auxcode.com>
2021-01-09 21:31:15 +0200

    Added the first 3 options and some helper functions

--- main.cpp
+++ main.cpp
@@ -18,14 +18,16 @@
 using namespace std;
 
 void chooseOperation(int &operationNumber);
-void multiplyMatrixByScalar();
-void multiplyMatrixByMatrix();
-void findTheDeterminant();
+void multiplyMatrixByScalar(int &rows, int &cols, int &scalar, double **matrix);
+void multiplyMatrixByMatrix(int &rows, int &cols, int &secondRows, int &secondCols, double **matrix, double **secondMatrix);
+void findTheDeterminant(int &rows, double **matrix);
 void divideMatrixByScalar();
 void findTheInverseMatrix();
 void matrixTransposition();
-void inputAgainIfLowerThanOne();
-
+void inputValueGreaterThanZero(int &number);
+double **inputMatrix(int &rows, int &cols);
+void printMatrix(double **matrix, int &rows, int &cols);
+void inputValueBetween2and4(int &number);
 
 int main()
 {
@@ -39,53 +41,192 @@ int main()
   }
   cout << "Choose an operation you want to use!" << endl;
   cout << textFile.rdbuf();
+  cout << "Operation: ";
   cin >> operationNumber;
+  cout << endl;
   if (operationNumber > 6 || operationNumber < 0)
   {
     cout << "Not valid operation number!";
     return 0;
   }
   chooseOperation(operationNumber);
-
+  textFile.close();
   return 0;
 }
 
-void inputAgainIfLowerThanOne(int &number)
+// Input matrix values
+double **inputMatrix(int &rows, int &cols)
 {
-  cout << "Input must be one or greater!\nInput again: ";
+  double **matrix;
+  double number;
+  matrix = new double *[rows];
+
+  for (int i = 0; i < rows; i++)
+  {
+    matrix[i] = new double[cols];
+
+    for (int j = 0; j < cols; j++)
+    {
+      cin >> number;
+      matrix[i][j] = number;
+    }
+  }
+  return matrix;
+}
+
+// Prints a matrix
+void printMatrix(double **matrix, int &rows, int &cols)
+{
+  cout << endl;
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      cout << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+// Input numbers for cols and rows
+void inputValueGreaterThanZero(int &number)
+{
   cin >> number;
+  while (number < 1)
+  {
+    cout << "Value must be one or greater!\nInput again: ";
+    cin >> number;
+    cout << endl;
+  }
   cout << endl;
 }
 
-void multiplyMatrixByScalar()
+// Input number for determinat size
+void inputValueBetween2and4(int &number)
 {
-  int scalar, matrixRows, matrixCols;
-  cout << "Input the scalar: ";
+  cin >> number;
+  while (number < 2 || number > 4)
+  {
+    cout << "Value not in the range!\nInput again: ";
+    cin >> number;
+    cout << endl;
+  }
+  cout << endl;
+}
+
+void multiplyMatrixByScalar(int &rows, int &cols, int &scalar, double **matrix)
+{
+  cout << "Input scalar: ";
   cin >> scalar;
   cout << endl;
-  cout << "Input the matrix rows: ";
-  cin >> matrixRows;
-  cout << endl;
-  while (matrixRows < 1)
+  cout << "Input matrix rows: ";
+  inputValueGreaterThanZero(rows);
+  cout << "Input matrix columns: ";
+  inputValueGreaterThanZero(cols);
+  cout << "Input matrix values:\n";
+  matrix = inputMatrix(rows, cols);
+
+  // multiplication logic
+  for (int i = 0; i < rows; i++)
   {
-    inputAgainIfLowerThanOne(matrixRows);
+    for (int j = 0; j < cols; j++)
+    {
+      matrix[i][j] *= scalar;
+    }
   }
-  cout << "Input the matrix columns: ";
-  cin >> matrixCols;
-  cout << endl;
-  while (matrixCols < 1)
+
+  printMatrix(matrix, rows, cols);
+
+  for (int i = 0; i < rows; i++)
   {
-    inputAgainIfLowerThanOne(matrixCols);
+    delete[] matrix[i];
   }
-  
+  delete[] matrix;
 }
 
-void multiplyMatrixByMatrix()
+void multiplyMatrixByMatrix(int &rows, int &cols, int &secondRows, int &secondCols, double **matrix, double **secondMatrix)
 {
+  double **multiplicationProduct;
+
+  cout << "Input first matrix rows: ";
+  inputValueGreaterThanZero(rows);
+  cout << "Input first matrix columns and second matrix rows: ";
+  inputValueGreaterThanZero(cols);
+  secondRows = cols;
+  cout << "Input second matrix columns: ";
+  inputValueGreaterThanZero(secondCols);
+  cout << "Input first matrix values:\n";
+  matrix = inputMatrix(rows, cols);
+  cout << "Input second matrix values:\n";
+  secondMatrix = inputMatrix(secondRows, secondCols);
+
+  // initializing the multiplication product matrix
+  multiplicationProduct = new double *[rows];
+  for (int i = 0; i < rows; i++)
+  {
+    multiplicationProduct[i] = new double[secondCols];
+
+    for (int j = 0; j < secondCols; j++)
+    {
+      matrix[i][j] = 0;
+    }
+  }
+
+  // multiplication logic
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < secondCols; j++)
+    {
+      for (int k = 0; k < cols; k++)
+      {
+        multiplicationProduct[i][j] += matrix[i][k] * secondMatrix[k][j];
+      }
+    }
+  }
+
+  printMatrix(multiplicationProduct, rows, secondCols);
+
+  for (int i = 0; i < rows; i++)
+  {
+    delete[] matrix[i];
+    delete[] multiplicationProduct[i];
+  }
+  delete[] matrix;
+  delete[] multiplicationProduct;
+
+  for (int i = 0; i < secondRows; i++)
+  {
+    delete[] secondMatrix[i];
+  }
+  delete[] secondMatrix;
 }
 
-void findTheDeterminant()
+void findTheDeterminant(int &rows, double **matrix)
 {
+  cout << "Input matrix size. Value can be 2, 3 or 4 ";
+  inputValueBetween2and4(rows);
+  cout << "Input matrix values:\n";
+  matrix = inputMatrix(rows, rows);
+
+  // Finding determinant logic. TODO implement smarter if there is time.
+  if (rows == 2)
+  {
+    cout << matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+  }
+  else if (rows == 3)
+  {
+    cout << matrix[0][0] * matrix[1][1] * matrix[2][2] + matrix[0][1] * matrix[1][2] * matrix[2][0] + matrix[0][2] * matrix[1][0] * matrix[2][1] - matrix[0][2] * matrix[1][1] * matrix[2][0] - matrix[0][1] * matrix[1][0] * matrix[2][2] - matrix[0][0] * matrix[1][2] * matrix[2][1];
+  }
+  else if (rows == 4)
+  {
+    cout << matrix[0][0] * matrix[1][1] * matrix[2][2] * matrix[3][3] + matrix[0][1] * matrix[1][2] * matrix[2][3] * matrix[3][0] + matrix[0][2] * matrix[1][3] * matrix[2][0] * matrix[3][1] + matrix[0][3] * matrix[1][0] * matrix[2][1] * matrix[3][2] - matrix[0][3] * matrix[1][2] * matrix[2][1] * matrix[3][0] - matrix[0][2] * matrix[1][1] * matrix[2][0] * matrix[3][3] - matrix[0][1] * matrix[1][0] * matrix[2][3] * matrix[3][2] - matrix[0][0] * matrix[1][3] * matrix[2][2] * matrix[3][1];
+  }
+
+  for (int i = 0; i < rows; i++)
+  {
+    delete[] matrix[i];
+  }
+  delete[] matrix;
 }
 
 void divideMatrixByScalar()
@@ -102,17 +243,21 @@ void matrixTransposition()
 
 void chooseOperation(int &operationNumber)
 {
+  int rows, cols, scalar, secondRows, secondCols;
+  double **matrix;
+  double **secondMatrix;
+
   if (operationNumber == 1)
   {
-    multiplyMatrixByScalar();
+    multiplyMatrixByScalar(rows, cols, scalar, matrix);
   }
   else if (operationNumber == 2)
   {
-    multiplyMatrixByMatrix();
+    multiplyMatrixByMatrix(rows, cols, secondRows, secondCols, matrix, secondMatrix);
   }
   else if (operationNumber == 3)
   {
-    findTheDeterminant();
+    findTheDeterminant(rows, matrix);
   }
   else if (operationNumber == 4)
   {
